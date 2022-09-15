f11974d22e3a6d05d88888b90024d578f0b56d76
Ugur Aytekin <uur.aytekin@gmail.com>
2022-09-15 22:45:40 +0300

    tests added

---
+++ src/test/com/uguraytekin/TestHopper.java
@@ -0,0 +1,42 @@
+package test.com.uguraytekin;
+
+import com.uguraytekin.Graph;
+import com.uguraytekin.Obstacle;
+import com.uguraytekin.Square;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: Ugur Aytekin
+ * @create: 15.09.2022
+ */
+public class TestHopper {
+
+
+    @Test
+    public void test1() {
+
+        List<Obstacle> obstacleList = Collections.singletonList(new Obstacle(1, 4, 2, 3));
+
+        Graph graph = new Graph(5, 5, obstacleList, new Square(4, 0), new Square(4, 4));
+
+        assertEquals("Optimal solution takes 7 hops.", graph.calculateShortestPath());
+    }
+
+    @Test
+    public void test2() {
+
+        List<Obstacle> obstacleList = Arrays.asList(
+                new Obstacle(1, 1, 0, 2),
+                new Obstacle(0, 2, 1, 1));
+
+        Graph graph = new Graph(3, 3, obstacleList, new Square(0, 0), new Square(2, 2));
+
+        assertEquals("No solution.", graph.calculateShortestPath());
+    }
+}
